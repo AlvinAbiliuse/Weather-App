cfd4615324e0fee7c5d7b6716631060afd8318df
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-01-31 01:35:30 +0400

    added starting html files in template.html and created function to call api in index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 console.log("hello world");
+let key = "Y3NCRY3KKECRAVJVMRBG92VWC";
+let locationQuery = "";
+
+async function getData() {
+	let response = await fetch();
+	console.log(response);
+}
+
+getData();
