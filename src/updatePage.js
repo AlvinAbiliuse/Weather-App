dc4025c33587ed18a66a1674623d3d0e1d12603a
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-02-02 03:11:20 +0400

    progress

--- src/updatePage.js
+++ src/updatePage.js
@@ -1,15 +1,23 @@
 let topContainer = document.querySelector(".topContainer");
 let mainContents = document.querySelector(".mainContents");
 
-function createCards(data) {
+function createCards(data, images) {
 	let card = document.createElement("div");
 	card.className = "card";
-	let h2 = document.createElement("h2");
+	let leftContents = document.createElement("div");
+	leftContents.className = "leftContents";
+	let h3 = document.createElement("h3");
 	let p = document.createElement("p");
+
+	card.appendChild(leftContents);
+	card.appendChild(rightContents);
 	return card;
 }
 function mainCard(data, images) {
-	topContainer.appendChild(createCards(data, images));
+	let h2 = document.createElement("h2");
+	h2.textContent = data.address;
+	topContainer.appendChild(h2);
+	topContainer.appendChild(createCards(data.currentConditions, images));
 }
 
 function collapsedCards(data, images) {
@@ -17,11 +25,13 @@ function collapsedCards(data, images) {
 	openBtn.value = "show more";
 	openBtn.className = "showMore";
 	mainContents.appendChild(openBtn);
+
 	let mainDiv = document.createElement("div");
 	mainDiv.className = "hidden";
 	for (let i in data.days) {
 		mainDiv.appendChild(createCard(data.days[i], images));
 	}
+
 	mainContents.appendChild(mainDiv);
 }
 
