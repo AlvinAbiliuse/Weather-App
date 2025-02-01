85ecf8983a862246bd12b7f2f2a7e586b523bb08
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-02-01 05:03:08 +0400

    imported the images and exported them as an array from exports.js

---
+++ src/exports.js
@@ -0,0 +1,47 @@
+import clearday from "./images/clear-day.svg";
+import clearnight from "./images/clear-night.svg";
+import cloudy from "./images/cloudy.svg";
+import fog from "./images/fog.svg";
+import hail from "./images/hail.svg";
+import partlycloudyday from "./images/partly-cloudy-day.svg";
+import partlycloudynight from "./images/partly-cloudy-night.svg";
+import rain from "./images/rain.svg";
+import rainsnow from "./images/rain-snow.svg";
+import rainsnowshowersday from "./images/rain-snow-showers-day.svg";
+import rainsnowshowersnight from "./images/rain-snow-showers-night.svg";
+import showersday from "./images/showers-day.svg";
+import showersnight from "./images/showers-night.svg";
+import sleet from "./images/sleet.svg";
+import snow from "./images/snow.svg";
+import snowshowersday from "./images/snow-showers-day.svg";
+import snowshowersnight from "./images/snow-showers-night.svg";
+import thunder from "./images/thunder.svg";
+import thunderrain from "./images/thunder-rain.svg";
+import thundershowersday from "./images/thunder-showers-day.svg";
+import thundershowersnight from "./images/thunder-showers-night.svg";
+import wind from "./images/wind.svg";
+
+export const imageList = [
+	clearday,
+	clearnight,
+	cloudy,
+	fog,
+	hair,
+	partlycloudyday,
+	partlycloudynight,
+	rain,
+	rainsnow,
+	rainsnowshowersday,
+	rainsnowshowersnight,
+	showersday,
+	showersnight,
+	sleet,
+	snow,
+	snowshowersday,
+	snowshowersnight,
+	thunder,
+	thunderrain,
+	thundershowersday,
+	thundershowersnight,
+	wind,
+];
