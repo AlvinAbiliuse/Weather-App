0050d78bd1b01b107227af200746fde194cf5317
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-02-02 03:41:09 +0400

    styled the cards

--- src/updatePage.js
+++ src/updatePage.js
@@ -8,16 +8,16 @@ function createCards(data, images) {
 	let rightContents = document.createElement("div");
 	leftContents.className = "leftContents";
 	rightContents.className = "rightContents";
-	let h3 = document.createElement("h3");
-	let datetime = document.createElement("p");
+	let date = document.createElement("h3");
+	let conditions = document.createElement("p");
 	let temp = document.createElement("p");
 
-	h3.textContent = "Conditions: " + data.conditions;
-	datetime.textContent = new Date(data.datetimeEpoch);
+	date.textContent = "Date: " + new Date(data.datetimeEpoch);
+	conditions.textContent = "Conditions: " + data.conditions;
 	temp.textContent = "Temperature: " + data.temp;
 
-	leftContents.appendChild(h3);
-	leftContents.appendChild(datetime);
+	leftContents.appendChild(date);
+	leftContents.appendChild(conditions);
 	leftContents.appendChild(temp);
 
 	let img = document.createElement("img");
@@ -39,12 +39,12 @@ function mainCard(data, images) {
 
 function collapsedCards(data, images) {
 	let openBtn = document.createElement("button");
-	openBtn.value = "show more";
+	openBtn.textContent = "show more";
 	openBtn.className = "showMore";
 	mainContents.appendChild(openBtn);
 
 	let mainDiv = document.createElement("div");
-	mainDiv.className = "hidden";
+	mainDiv.className = "mainDiv hidden";
 	for (let i in data.days) {
 		mainDiv.appendChild(createCards(data.days[i], images));
 	}
