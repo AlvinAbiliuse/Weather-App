ad3edfd43fb8d765288d9b26f05ac11a60adb315
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-02-02 01:50:39 +0400

    added eventlistener to index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import images from "./exports.js";
 
-let key = "Y3NCRY3KKECRAVJVMRBG92VWC";
-let locationQuery = "dubai";
-let url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${locationQuery}?key=${key}`;
-
 let data;
-async function getData() {
+
+async function getData(locationQuery) {
+	let key = "Y3NCRY3KKECRAVJVMRBG92VWC";
+	let url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${locationQuery}?key=${key}`;
 	let response = await fetch(url);
 	data = await response.json();
-	console.log(data);
+	return data;
 	//imgEl.src = images[data.currentConditions.icon.split("-").join("")];
 }
 
 let imgEl = document.querySelector(".image");
 
-getData();
+let formEl = document.querySelector("form");
+let input = formEl.querySelector("input");
+
+formEl.addEventListener("submit", (e) => {
+	data = getData(input.value);
+	data.then((data) => console.log(data));
+	input.value = "";
+	e.preventDefault();
+});
