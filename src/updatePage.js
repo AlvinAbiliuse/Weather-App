dbc0c35cd774f83698b23432581fe566adfd7e42
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-02-02 07:37:20 +0400

    removed old function from updatePage.js

--- src/updatePage.js
+++ src/updatePage.js
@@ -2,39 +2,6 @@ import createCards from "./createCard.js";
 
 let topContainer = document.querySelector(".topContainer");
 let mainContents = document.querySelector(".mainContents");
-/*
-function createCards(data, images) {
-	let card = document.createElement("div");
-	card.className = "card";
-	let leftContents = document.createElement("div");
-	let rightContents = document.createElement("div");
-	leftContents.className = "leftContents";
-	rightContents.className = "rightContents";
-	let date = document.createElement("h3");
-	let conditions = document.createElement("p");
-	let temp = document.createElement("p");
-	let currentDate = Date.now();
-	if (new Date(data.datetime) == "Invalid Date") {
-		date.textContent = "Time: " + data.datetime;
-	} else {
-		date.textContent = "Date: " + new Date(data.datetime);
-	}
-	conditions.textContent = "Conditions: " + data.conditions;
-	temp.textContent = "Temperature: " + data.temp + " ℉";
-
-	leftContents.appendChild(date);
-	leftContents.appendChild(conditions);
-	leftContents.appendChild(temp);
-
-	let img = document.createElement("img");
-	img.src = images[data.icon.split("-").join("")];
-
-	rightContents.appendChild(img);
-
-	card.appendChild(leftContents);
-	card.appendChild(rightContents);
-	return card;
-} */
 
 function mainCard(data, images) {
 	let currentDate = data.days[0].datetime;
