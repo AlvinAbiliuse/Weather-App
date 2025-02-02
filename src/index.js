26efa52ac4f368b0463c2179b4f67616f5f8b84f
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-02-02 05:49:23 +0400

    added loader className change in index.js

--- src/index.js
+++ src/index.js
@@ -7,8 +7,13 @@ let formEl = document.querySelector("form");
 let input = formEl.querySelector("input");
 
 formEl.addEventListener("submit", (e) => {
+	document.querySelector(".loader").className = "loader";
 	data = getData(input.value);
-	data.then((data) => updatePage(data, images));
+	data
+		.then((data) => updatePage(data, images))
+		.then(() => {
+			document.querySelector(".loader").className = "loader hidden";
+		});
 	input.value = "";
 	e.preventDefault();
 });
